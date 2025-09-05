b9ceba2466d99c7f2a3718f3e2b4976fa24c6aaf
wx <547036318@qq.com>
2025-09-05 21:59:58 +0800

    add：umi

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
 
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
+import { healthUsingGet } from '@/api/mainController.ts'
+
+healthUsingGet().then((response) => {
+  console.log(response)
+})
+
 </script>
 
 <style scoped>
